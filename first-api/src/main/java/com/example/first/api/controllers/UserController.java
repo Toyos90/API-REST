7438da307b81835ddf964c40c10bf6c02ab9515d
javier <ertoyos@gmail.com>
2023-08-16 01:18:33 +0200

    edit

--- first-api/src/main/java/com/example/first/api/controllers/UserController.java
+++ first-api/src/main/java/com/example/first/api/controllers/UserController.java
@@ -2,6 +2,10 @@ package com.example.first.api.controllers;
 
 import com.example.first.api.services.UserService;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class UserController extends UserService {
 }
