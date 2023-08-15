30270a2f48a437be8f6b10fa2ff0d54adb135670
javier <ertoyos@gmail.com>
2023-08-16 00:09:42 +0200

    add

---
+++ carta.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-08-2023 a las 00:04:20
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `restaurante`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carta`
+--
+
+CREATE TABLE `carta` (
+  `id` int(11) NOT NULL,
+  `Precio` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `carta`
+--
+
+INSERT INTO `carta` (`id`, `Precio`) VALUES
+(0, '50'),
+(0, '55'),
+(0, '10'),
+(0, '6'),
+(0, '18'),
+(0, '14'),
+(0, '10'),
+(0, '12'),
+(0, '10'),
+(0, '14'),
+(0, '14'),
+(0, '12'),
+(0, '18'),
+(0, '12'),
+(0, '12'),
+(0, '14'),
+(0, '18'),
+(0, '18'),
+(0, '18'),
+(0, '0');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ first-api/src/main/java/com/example/first/api/controllers/UserController.java
@@ -0,0 +1,7 @@
+package com.example.first.api.controllers;
+
+import com.example.first.api.services.UserService;
+import org.springframework.data.repository.CrudRepository;
+
+public class UserController extends UserService {
+}
