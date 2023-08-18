ead09b5e4636b1bbdaf832fdde4f0c74e02a2b57
javier <ertoyos@gmail.com>
2023-08-18 11:47:23 +0200

    add controllers

--- first-api/src/main/java/com/example/first/api/controllers/UserController.java
+++ first-api/src/main/java/com/example/first/api/controllers/UserController.java
@@ -1,11 +1,46 @@
-package com.example.first.api.controllers;
+package java_spring.api_crud.controllers;
 
-import com.example.first.api.services.UserService;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java_spring.api_crud.models.UserModel;
+import java_spring.api_crud.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
-public class UserController extends UserService {
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ArrayList<UserModel> getUsers(){
+        return this.userService.getUsers();
+    }
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel user){
+        return this.userService.saveUser(user);
+    }
+
+    @GetMapping(path ="/{id}")
+    public Optional<UserModel> getUserById(@PathVariable Long id){
+        return this.userService.getById(id);
+    }
+
+    @PutMapping(path ="/{id}")
+    public UserModel updateUserById(@RequestBody UserModel request, @PathVariable("id") Long id){
+        return this.userService.updateById(request, id);
+    }
+
+    @DeleteMapping(path ="/{id}")
+    public String deleteById(@PathVariable ("id") Long id){
+        boolean ok = this.userService.deleteUser(id);
+
+        if(ok){
+            return "El usuario con id " + id + " ha sido aniquilado.";
+        } else{
+            return "Uisss, problemas en el paraíso, vuelve a intentarlo.";
+        }
+    }
 }
